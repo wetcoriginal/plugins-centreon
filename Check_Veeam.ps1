912ff0f80a6d1566154e74514b7cec2399f1aba1
W&C <82039108+wetcoriginal@users.noreply.github.com>
2023-05-19 12:08:17 +0200

    Create Check_Veeam.ps1

---
+++ Check_Veeam.ps1
@@ -0,0 +1,153 @@
+$WarningPreference = 'SilentlyContinue'
+Add-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue
+function CheckOneJob {
+    $JobCheck=get-vbrjob -Name $args[0]
+
+        if($global:OutMessageTemp -ne ""){$global:OutMessageTemp+="`r`n"}
+
+        if($JobCheck.isScheduleEnabled -eq $false){ # Disabled job -> WARNING
+            if($DisabledJobs -eq $true){
+                $global:OutMessageTemp+=" WARNING - Le job '"+$JobCheck.Name+"' est desactive "
+                $global:WarningDisabledCount++ 
+                if($global:ExitCode -lt 2){$global:ExitCode=1} # if no previous Critical status then switch to WARNING
+                }
+            }
+        else  # The job is enabled
+        {
+            $lastStatus=$JobCheck.GetLastResult()
+            if($($JobCheck.findlastsession()).State -eq "Working"){
+                $global:OutMessageTemp+="OK - Le job "+$JobCheck.Name+" est en cours de sauvegarde"
+                $global:OkCount++ 
+            }
+            else {
+                if($lastStatus -ne "Success"){ # Failed or None->never run before (probaly a newly created job)
+                    if($lastStatus -eq "none"){
+                        $global:OutMessageTemp+="WARNING: Le job "+$JobCheck.Name+" n a jamais ete execute"
+                        $global:WarningCount++ 
+                        if($global:ExitCode -ne 2) {$global:ExitCode=1}
+                    }
+                    elseif($lastStatus -eq "Warning"){
+                        $global:OutMessageTemp+="WARNING - Le job "+$JobCheck.Name+" s est termine avec des messages d'alertes"
+                        $global:WarningCount++ 
+                        if($global:ExitCode -ne 2) {$global:ExitCode=1}
+                    }
+                    else {
+                        $global:OutMessageTemp+="CRITICAL - Le job "+$JobCheck.Name+" a echoue"
+                        $global:CriticalCount++ 
+                        $global:ExitCode=2
+                       }
+                }
+                else
+                {  
+
+                    #$global:OutMessageTemp+="QUoiquece:"+$DiffTime.TotalDays
+                    $EstRun=get-date
+                    if($JobCheck.IsBackup -eq $true)
+                    {
+                        $LastRunSession=Get-VBRSession -Job $JobCheck -Last
+                        $LastRun=$LastRunSession.CreationTime
+                        $DiffTime=new-timespan $LastRun $EstRun
+
+                    }
+                    else
+                    {
+                        $LastRun=$JobCheck.ScheduleOptions.LatestRunLocal
+                        $DiffTime=new-timespan $LastRun $EstRun
+                    }
+                    
+                    if (($JobCheck.IsBackup -eq $true) -and ($DiffTime.TotalDays -gt 1))
+                    {
+                        $global:ExitCode=2
+                        $global:OutMessageTemp+="CRITICAL - Le job "+$JobCheck.Name+" n a pas ete execute lors de la derniere journee"
+                        $global:CriticalCount++
+                    }
+                    
+                    else
+                    {
+                        if(($JobCheck.IsReplica -eq $true) -and ($DiffTime.TotalHours -gt 2) )
+                        {
+                            $global:ExitCode=2
+                            $global:OutMessageTemp+="CRITICAL - La replication "+$JobCheck.Name+" n a pas ete execute lors des 2 dernieres heures"
+                            $global:CriticalCount++
+                        }
+                        else
+                        {
+                            $global:OutMessageTemp+="OK - "
+                            $global:OutMessageTemp+=$JobCheck.Name+" "
+                            $global:OutMessageTemp+="execute le "+("{0:D2}" -f $LastRun.Day)+"/"+("{0:D2}" -f $LastRun.Month)+"/"+$LastRun.year+" "+("{0:D2}" -f $LastRun.hour)+":"+("{0:D2}" -f $LastRun.Minute)+" "
+                            $global:OkCount++
+                        }
+                    }
+                }
+            }
+        }
+ }
+
+######################################################
+#           Main loop (well, not exactly a loop)     #
+######################################################
+
+$nextIsJob=$false
+$oneJob=$false
+$jobToCheck=""
+$WrongParam=$false
+$DisabledJobs=$true
+$global:OutMessageTemp=""
+$global:OutMessage=""
+$global:Exitcode=""
+
+#Ajout de variables pour compter le nombre d'erreurs
+$global:WarningDisabledCount=0
+$global:WarningCount=0
+$global:CriticalCount=0
+$global:OkCount=0
+$TotalCount=0
+$global:Graph=""
+
+if( $args.Length -ge 1)
+ {
+     foreach($value in $args) {
+       if($nextIsJob -eq $true) { # parameter coming after -j switch
+            if(($value.Length -eq 2) -and ($value.substring(0,1) -eq '-')){
+                $WrongParam=$true
+                }
+            $nextIsJob=$false
+            $jobToCheck=$value
+            $onejob=$true
+            }
+       elseif($value -eq '-j') { # -j -> check only one job and its name goes in the following parameter (default is to check all backup jobs)
+            $nextIsJob=$true
+            }
+       elseif($value -eq '-d') { # -d -> Do not warn for disabled jobs (default is to warn)
+            $DisabledJobs=$false
+            }
+       else {$WrongParam=$true}
+       }
+  }
+
+if($WrongParam -eq $true){
+    write-host "Wrong parameters"
+    write-host "Syntax: Check_Veeam_Jobs [-j JobNameToCheck] [-d]"
+    write-host "       -j switch to check only one job (default is to check all backup jobs)"
+    Write-Host "       -d switch to not inform when there is any disabled job"
+    exit 1
+    }
+
+$VJobList=get-vbrjob
+$ExitCode=0
+
+IF($oneJob -eq $true){
+    CheckOneJob($jobToCheck)}
+else {
+    foreach($Vjob in $VJobList){
+        CheckOneJob($Vjob.Name)
+    }
+}
+#Ajout du nombre total d'erreur dÃ©tectÃ©es
+$TotalCount=$global:WarningDisabledCount + $global:WarningCount + $global:CriticalCount + $global:OkCount
+$global:OutMessage="TOTAL=>" + $TotalCount + " / OK=>" + $global:OkCount + " / CRITICAL=>" + $global:CriticalCount + " / DISABLE=>" + $global:WarningDisabledCount + " / WARNING=>" + $global:WarningCount
+#Ajout variable Graph pour visualisation graphique sur centreon
+$global:Graph=" |  Ok=" + $global:OkCount + " Warning=" + $global:WarningCount + " Critical=" + $global:CriticalCount
+$global:OutMessage+="`r`n" + $global:OutMessageTemp + $global:Graph
+write-host $global:OutMessage
+exit $global:Exitcode
