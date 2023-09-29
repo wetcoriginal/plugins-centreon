95962a5097aee5db197c7c067030ba4940dc1940
W&C <82039108+wetcoriginal@users.noreply.github.com>
2023-09-29 09:35:01 +0200

    Update Script_Veeam_Office365.ps1

--- Script_Veeam_Office365.ps1
+++ Script_Veeam_Office365.ps1
@@ -3,46 +3,46 @@
 function CheckOneJob {
 $JobCheck=Get-VBOJob -Name $args[0]
 $lastStatus=$JobCheck | Get-VBOJobSession -Last | Foreach-Object {$_.Status}
-$CreationJobTime=$JobCheck | Get-VBOJobSession -Last | Foreach-Object {$_.EndTime}
+$CreationJobTime=$JobCheck | Get-VBOJobSession -Last | Foreach-Object {$_.CreationTime}
 $DisabledJobs=$JobCheck | Foreach-Object {$_.IsEnabled}
-$Avant=$JobCheck | Get-VBOJobSession -Last | Foreach-Object {$_.EndTime}
+$Avant=$JobCheck | Get-VBOJobSession -Last | Foreach-Object {$_.CreationTime}
 $Apres=Get-Date
 $TempsEcoulee=$Apres-$Avant
-$LimitTemps="23:59:00.0000000" #Valeur -> Si le job est en Running depuis + de 24h -> Warning 
-
+$LimitTemps="23:59:59.0000000"
+ 
 if($global:OutMessageTemp -ne ""){$global:OutMessageTemp+="`r`n"}
-if($JobCheck.isEnabled -eq $false){
+if($JobCheck.isEnabled -eq $false){ # Disabled job -> WARNING
 if($DisabledJobs -ne $true){
 $global:OutMessageTemp+="WARNING - Le job '"+$JobCheck.Name+"' est desactive "
-$global:WarningDisabledCount++
-if($global:ExitCode -lt 2){$global:ExitCode=1}
+$global:WarningDisabledCount++ #exo
+if($global:ExitCode -lt 2){$global:ExitCode=1} # if no previous Critical status then switch to WARNING
 }
 }
-else
+else # The job is enabled
 {
-if($lastStatus -eq "Running" -and $TempsEcoulee -gt $LimitTemps){ # Si le job est en Running depuis + de 24h -> Warning 
+if($lastStatus -eq "Running" -and $TempsEcoulee -gt $LimitTemps){
 $global:OutMessageTemp+="WARNING: Le job "+$JobCheck.Name+" est en cours depuis $TempsEcoulee minutes"
-$global:WarningCount++
+$global:WarningCount++ #exo
 }
 elseif($lastStatus -eq "Running"){
 $global:OutMessageTemp+="OK - Le job "+$JobCheck.Name+" est en cours de sauvegarde"
-$global:OkCount++
+$global:OkCount++ #exo
 }
 else {
-if($lastStatus -ne "Success"){
+if($lastStatus -ne "Success"){ # Failed or None->never run before (probaly a newly created job)
 if($lastStatus -eq "none"){
 $global:OutMessageTemp+="WARNING: Le job "+$JobCheck.Name+" n a jamais ete execute"
-$global:WarningCount++
+$global:WarningCount++ #exo
 if($global:ExitCode -ne 2) {$global:ExitCode=1}
 }
 elseif($lastStatus -eq "Warning"){
 $global:OutMessageTemp+="WARNING - Le job "+$JobCheck.Name+" s est termine avec des messages d'alertes"
-$global:WarningCount++
+$global:WarningCount++ #exo
 if($global:ExitCode -ne 2) {$global:ExitCode=1}
 }
 else {
 $global:OutMessageTemp+="CRITICAL - Le job "+$JobCheck.Name+" a echoue"
-$global:CriticalCount++
+$global:CriticalCount++ #exo
 $global:ExitCode=2
 }
 }
@@ -53,7 +53,7 @@ if (($JobCheck.IsBackup -eq $true) -and ($DiffTime.TotalDays -gt 1))
 {
 $global:ExitCode=2
 $global:OutMessageTemp+="CRITICAL - Le job "+$JobCheck.Name+" n a pas ete execute lors de la derniere journee"
-$global:CriticalCount++
+$global:CriticalCount++ #exo
 }
  
 else
@@ -62,21 +62,25 @@ if(($JobCheck.IsReplica -eq $true) -and ($DiffTime.TotalHours -gt 2) )
 {
 $global:ExitCode=2
 $global:OutMessageTemp+="CRITICAL - La replication "+$JobCheck.Name+" n a pas ete execute lors de la derniere journee"
-$global:CriticalCount++
+$global:CriticalCount++ #exo
 }
 else
 {
 $global:OutMessageTemp+="OK - "
 $global:OutMessageTemp+=$JobCheck.Name+" "
 $global:OutMessageTemp+="execute avec succes le $CreationJobTime"
-$global:OkCount++
+$global:OkCount++ #exo
 }
 }
 }
 }
 }
 }
-
+ 
+######################################################
+# Main loop (well, not exactly a loop) #
+######################################################
+ 
 $nextIsJob=$false
 $oneJob=$false
 $jobToCheck=""
@@ -84,18 +88,18 @@ $WrongParam=$false
 $global:OutMessageTemp=""
 $global:OutMessage=""
 $global:Exitcode=""
-#Ajout de variables pour compter le nombre d'erreurs
+#exo - Ajout de variables pour compter le nombre d'erreurs
 $global:WarningDisabledCount=0
 $global:WarningCount=0
 $global:CriticalCount=0
 $global:OkCount=0
 $TotalCount=0
-
-
+ 
+ 
 if( $args.Length -ge 1)
 {
 foreach($value in $args) {
-if($nextIsJob -eq $true) {
+if($nextIsJob -eq $true) { # parameter coming after -j switch
 if(($value.Length -eq 2) -and ($value.substring(0,1) -eq '-')){
 $WrongParam=$true
 }
@@ -103,10 +107,10 @@ $nextIsJob=$false
 $jobToCheck=$value
 $onejob=$true
 }
-elseif($value -eq '-j') {
+elseif($value -eq '-j') { # -j -> check only one job and its name goes in the following parameter (default is to check all backup jobs)
 $nextIsJob=$true
 }
-elseif($value -eq '-d') {
+elseif($value -eq '-d') { # -d -> Do not warn for disabled jobs (default is to warn)
 $DisabledJobs=$false
 }
 else {$WrongParam=$true}
@@ -128,9 +132,23 @@ foreach($Vjob in $VJobList){
 CheckOneJob($Vjob.Name)
 }
 }
-#Ajout du nombre total d'erreur détectées
+#exo - Ajout du nombre total d'erreur détectées
 $TotalCount=$global:WarningDisabledCount + $global:WarningCount + $global:CriticalCount + $global:OkCount
 $global:OutMessage="TOTAL=>" + $TotalCount + " / OK=>" + $global:OkCount + " / CRITICAL=>" + $global:CriticalCount + " / DISABLE=>" + $global:WarningDisabledCount + " / WARNING=>" + $global:WarningCount
 $global:OutMessage+="`r`n" + $global:OutMessageTemp
+
+if($global:WarningCount =! 0){
+$global:Exitcode = 1
+}
+elseif($global:WarningDisabledCount =! 0){
+$global:Exitcode = 1
+}
+elseif($global:CriticalCount =! 0){
+$global:Exitcode = 2
+}
+else{
+$global:Exitcode = 0
+}
+
 write-host $global:OutMessage
 exit $global:Exitcode
